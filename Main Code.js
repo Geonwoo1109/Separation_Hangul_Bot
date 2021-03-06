6253a513c38b662d4e0aa1fd2b2af27dd6225c4c
Geonwoo1109 <74478101+Geonwoo1109@users.noreply.github.com>
2021-03-06 22:42:24 +0900

    Update Main Code.js

--- Main Code.js
+++ Main Code.js
@@ -1,12 +1,33 @@
+var big = "\u200d";
+
 function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
-  
-if (msg.startsWith (".분리 ")) {
+
+   if (msg.startsWith(".큰글씨 ")) {
+      var real1 = msg.substr(5);
+     
+     if (real1.length < 6) { // x-1개까지 허용
+       try {
+       for (i = 0; i < real1.length; i++) {
+         replier.reply (real1 [i] + big);
+       }
+       } catch (e) {
+         replire.reply (e);
+       }
+     } else {
+       replier.reply("5글자까지만 허용됩니다.");
+     }
+   }
+   
+     
+if (msg.startsWith ("..큰글씨 ")) {
 
    try {
-     var a = msg.substr(4).normalize("NFD").split("");
-     replier.reply (a);
-   }
+     var a = msg.substr(6).normalize("NFD").split("");
+     replier.reply (a[0] + a[2] + a[4] + big);
+   
    } catch (e) {
      replier.reply (e);
    }
+}
+   
 }
